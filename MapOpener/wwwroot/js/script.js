5a1f2a682496ac93f7a011ef3a80cead6aefa6c7
Zach Johnson <zlg23johnson@gmail.com>
2020-06-17 07:00:17 -0500

    Simple cross platform map open

---
+++ MapOpener/wwwroot/js/script.js
@@ -0,0 +1,9 @@
+﻿function mapSelector() {
+    if ((navigator.platform.indexOf('iPhone') != -1)
+        || navigator.platform.indexOf('iPad') != -1
+        || navigator.platform.indexOf('iPod') != -1) {
+        window.open("maps://maps.google.com/maps?daddr=13,17&amp;ll=");
+    } else {
+        window.open("https://maps.google.com/maps?daddr=13,17&amp;ll=");
+    }
+}
